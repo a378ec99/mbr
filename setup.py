a236f00df974c9ecdc18b0ff37793ccc26d904ee
a378ec99 <sebastian.ohse@mailbox.org>
2017-12-20 13:33:39 +0100

    readme extension fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='sebastian.ohse@mailbox.org',
     url='https://github.com/a378ec99/mbr',
     license='LICENSE.txt',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     keywords=('biological relevance', 'gene set enrichment', 'null distribution'),
     packages=find_packages(exclude=['tests']),
     install_requires=['scipy >= 0.19.0', 'numpy >= 1.11.3', 'h5py >= 2.6.0', 'matplotlib >= 1.5.3', 'seaborn >= 0.7.1']
