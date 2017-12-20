e08052a2379b8e81a72c835d1133bf0e41fff0e2
a378ec99 <sebastian.ohse@mailbox.org>
2017-12-20 13:44:26 +0100

    minor fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MBR',
+    name='mbr',
     version='0.1.0',
     description='Measure of Biological Relevance',
     author='Sebastian Ohse',
